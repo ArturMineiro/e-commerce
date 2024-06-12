59cf2e1c0d2f3b648a2c774de5ed24a71d078086
artur.mineiro <arturmineiro2@hotmail.com>
2024-06-12 14:01:50 -0300

    tamanho card

--- src/components/CardLaterais.tsx
+++ src/components/CardLaterais.tsx
@@ -1,7 +1,7 @@
 function CardLaterais() {
     return (
         <div className="card mb-4 d-flex flex-row">
-        <img src="/assets/homeimage1.jpg" className="card-img-left" alt="Card image" style={{ width: '40%' }} />
+        <img src="/assets/homeimage1.jpg" className="card-img-left" alt="Card image" style={{ width: '30%' }} />
         <div className="card-body">
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
